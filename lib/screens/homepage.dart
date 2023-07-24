05bb8f3c54652e4117c027099cfda7899dc89ea3
Maleesha Herath <malishaherath77@gmail.com>
2023-07-24 09:34:32 +0530

    create http requist

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/servies/api_servies.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ApiService service = ApiService();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    service.getMovies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/servies/api_servies.dart
+++ lib/servies/api_servies.dart
@@ -1,6 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:movie_app/models/movies_models.dart';
+
 class ApiService {
   final apiKey = "api_key=12e9267e1ffc75c78e425233f747a876";
   final popular = "https://api.themoviedb.org/3/movie/popular?";
 
-  void getMovies() {}
+  Future<void> getMovies() async {
+    Response response = await get(Uri.parse("$popular$apiKey"));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      List<dynamic> data = body["results"];
+      List<Movie> movies = data.map((movie) => Movie.fromJson(movie)).toList();
+      print(movies[0].title);
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
 }
