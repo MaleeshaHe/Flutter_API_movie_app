69ec1d39477211991bab319521344aef64a7f0d6
Maleesha Herath <malishaherath77@gmail.com>
2023-07-24 09:47:09 +0530

    get data to home page

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/models/movies_models.dart';
 import 'package:movie_app/servies/api_servies.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,20 +11,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ApiService service = ApiService();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    service.getMovies();
-  }
+  List<Movie> movies = [];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Maleesha"),
-      ),
-    );
+    return Scaffold(
+        body: FutureBuilder(
+      future: service.getMovies(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          movies = snapshot.data!;
+          return GridView.builder(
+            itemCount: movies.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                childAspectRatio: 0.65),
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.grey,
+              );
+            },
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
+    ));
   }
 }
--- lib/servies/api_servies.dart
+++ lib/servies/api_servies.dart
@@ -7,13 +7,13 @@ class ApiService {
   final apiKey = "api_key=12e9267e1ffc75c78e425233f747a876";
   final popular = "https://api.themoviedb.org/3/movie/popular?";
 
-  Future<void> getMovies() async {
+  Future<List<Movie>> getMovies() async {
     Response response = await get(Uri.parse("$popular$apiKey"));
     if (response.statusCode == 200) {
       Map<String, dynamic> body = jsonDecode(response.body);
       List<dynamic> data = body["results"];
       List<Movie> movies = data.map((movie) => Movie.fromJson(movie)).toList();
-      print(movies[0].title);
+      return movies;
     } else {
       throw Exception(response.statusCode);
     }
